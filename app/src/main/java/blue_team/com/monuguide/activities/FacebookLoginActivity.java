0bdb7ef165befaf52913fa0b6833ce18dc3b75fb
sedbars <sedbars94@gmail.com>
2017-03-20 21:41:12 +0400

    facebooklogin

---
+++ app/src/main/java/blue_team/com/monuguide/activities/FacebookLoginActivity.java
@@ -0,0 +1,23 @@
+package blue_team.com.monuguide.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import blue_team.com.monuguide.R;
+
+public class FacebookLoginActivity extends AppCompatActivity {
+//    private CallbackManager mCallbackManager;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_facebook_login);
+//        mCollbackManager = CallbackManager.Factory.create();
+//        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+//        loginButton.setReadPermissions("email","public_profile");
+//        loginButton.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>){
+//
+//        }
+//
+//    }
+}
